f6404e5cf6c922bc6eb652599bce25a93362d1e4
Sergio Wehbe <sergiowehbe@gmail.com>
2022-04-30 10:54:52 +0300

    Initial Commit

---
+++ MathematicalOperationsGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MathematicalOperationsGame
+//
+//  Created by Sergio Wehbe on 30/04/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
